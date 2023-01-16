746f6e6858cb40c208e50899c765fa22eee5ce15
xe36776 <borja.ruiz.contractor@bbva.com>
2023-01-16 12:24:54 +0100

    Solve DnaStrand kata kyu 7

---
+++ src/main/java/com/codewars/katas/kyu7/DnaStrand.java
@@ -0,0 +1,27 @@
+package com.codewars.katas.kyu7;
+
+import java.util.stream.Collectors;
+
+public class DnaStrand {
+	
+	public static String makeComplement(String dna) {	
+		return dna.chars().mapToObj(DnaStrand::complementOf).collect(Collectors.joining());
+	}
+	
+	public static String complementOf(int dnaInt) {
+		
+		char dnaChar = (char) dnaInt;
+		
+		String dnaComplement = null;
+		
+		switch(String.valueOf(dnaChar)) {
+			case "A": dnaComplement = "T"; break;
+			case "T": dnaComplement = "A"; break;
+			case "C": dnaComplement = "G"; break;
+			case "G": dnaComplement = "C"; break;
+		}
+		
+		return dnaComplement;
+		
+	}
+}
---
+++ src/test/java/com/codewars/katas/kyu7/DnaStrandTest.java
@@ -0,0 +1,22 @@
+package com.codewars.katas.kyu7;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DnaStrandTest {
+	
+    @Test
+    public void test01() {
+       assertEquals("TTTT", DnaStrand.makeComplement("AAAA"));
+    }
+    @Test
+    public void test02() {
+       assertEquals("TAACG", DnaStrand.makeComplement("ATTGC"));
+    }
+    @Test
+    public void test03() {
+       assertEquals("CATA", DnaStrand.makeComplement("GTAT"));
+    }    
+    
+}
