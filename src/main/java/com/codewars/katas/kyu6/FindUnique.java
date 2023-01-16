971372258ec2f795fc06a9cc396345d11233d941
Flashky <mevengar@gmail.com>
2023-01-16 15:37:39 +0100

    Add comments to FindUnique

--- src/main/java/com/codewars/katas/kyu6/FindUnique.java
+++ src/main/java/com/codewars/katas/kyu6/FindUnique.java
@@ -18,4 +18,11 @@ public class FindUnique {
     		.findFirst()
     		.get();
     }
+    
+    // More solutions: https://www.codewars.com/kata/585d7d5adb20cf33cb000235/solutions/java
+
+    // Downsides
+    // https://www.codewars.com/kata/585d7d5adb20cf33cb000235/solutions#5ecd0cc31dbe7e001fda5ba9:
+    // - groupingBy has a time complexity of O(nlog(n))(similar to sorting), so ultimately this isn't an effecient solution as you can solve it with a linear time complexity. 
+    
 }
