8d469eb00e83ce5b322f1659e32d192a217cf29d
Flashky <mevengar@gmail.com>
2023-01-17 12:54:57 +0100

    Solve HexToRGB kyu5 kata

---
+++ src/test/java/com/codewars/katas/kyu5/HexToRGBTest.java
@@ -0,0 +1,23 @@
+package com.codewars.katas.kyu5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HexToRGBTest {
+
+    @Test public void allOne() {
+        assertArrayEquals(HexToRGB.hexStringToRGB("#111111"), new int[] {17, 17, 17});
+    }
+  
+    @Test public void allZero() {
+        assertArrayEquals(HexToRGB.hexStringToRGB("#000000"), new int[] {0, 0, 0});
+    }
+  
+    @Test public void more() {
+        assertArrayEquals(HexToRGB.hexStringToRGB("#FF9933"), new int[] {255, 153, 51});
+        assertArrayEquals(HexToRGB.hexStringToRGB("#beaded"), new int[] {190, 173, 237});
+        assertArrayEquals(HexToRGB.hexStringToRGB("#Fa3456"), new int[] {250, 52, 86});
+    }
+
+}
