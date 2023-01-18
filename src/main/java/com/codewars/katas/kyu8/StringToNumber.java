40b0c5b5c33ce7e913f91ddd4f5cc45da503b5c2
Flashky <mevengar@gmail.com>
2023-01-18 19:16:05 +0100

    Add kyu 8 code

---
+++ src/main/java/com/codewars/katas/kyu8/StringToNumber.java
@@ -0,0 +1,10 @@
+package com.codewars.katas.kyu8;
+
+/**
+ * <strong>Kata:</strong> <a href="https://www.codewars.com/kata/544675c6f971f7399a000e79/solutions/java">Convert a String to a Number!</a>
+ */
+public class StringToNumber {
+	public static int stringToNumber(String str) {
+		return Integer.parseInt(str);
+	}
+}
